908c7ac4076513af4021a428c86f798d15a79db3
fahim-nexlent <152833148+fahim-nexlent@users.noreply.github.com>
2023-12-08 11:27:39 +0600

    feedback on user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,18 +1,27 @@
-interface User{
+// 1. Interface Naming
+// 2. Difference between Interface and Type
+// 3. Inferred Type
+// 4. Always use Arrow function
+// 5. Always validate arguments first to reduce function execution time
+// 6. Don't use ANY type
+
+interface IUser {
     id:number;
     userName:string;
     email:string;
-    postCount:2
+    postCount:2;
 }
 
-
-const usersArray:{id:number;userName:string;email:string,postCount:number}[]=[
+const usersArray=[
     {id:1,userName:"shamol",email:"shamol@gmail.com",postCount:2},   
     {id:2,userName:"shamol2",email:"shamol2@gmail.com",postCount:2},   
     {id:3,userName:"shamol3",email:"shamol3@gmail.com",postCount:2}   
 ]
 
 function getUser(userId: number){
+    if(!userId) {
+        return null;
+    }
     const getingUser=usersArray.filter(user=>user.id===userId)
     return getingUser;
 }
@@ -24,7 +33,7 @@ function findUser(userId: number){
     return getingUser;
 }
 
-function getAllUser(users:any[]) {
+function getAllUser(users:IUser[]) {
     return users;
 }
 
