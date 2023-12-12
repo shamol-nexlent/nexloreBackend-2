09fde84a2eda758b167b4041deed1248a7eb2c9a
shamolmojumder <shamolmojumder14@gmail.com>
2023-12-13 01:14:44 +0600

    user crud operation done

--- dist/users/user.js
+++ dist/users/user.js
@@ -1,25 +1,41 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeUser = exports.getAllUsers = exports.findUser = exports.getUser = void 0;
 const usersArray = [
     { id: 1, userName: "shamol", email: "shamol@gmail.com", postCount: 2 },
     { id: 2, userName: "shamol2", email: "shamol2@gmail.com", postCount: 2 },
     { id: 3, userName: "shamol3", email: "shamol3@gmail.com", postCount: 2 }
 ];
-function getUser(userId) {
-    const getingUser = usersArray.filter(user => user.id === userId);
-    return getingUser;
-}
-console.log("getuser", getUser(3));
-function findUser(userId) {
-    const getingUser = usersArray.filter(user => user.id === userId);
-    return getingUser;
-}
-function getAllUser(users) {
-    return users;
-}
-console.log("getalluser", getAllUser(usersArray));
-function removeUser(userId) {
-    const remove = usersArray.filter(user => user.id === userId);
-    return remove;
-}
-console.log("removeuser", removeUser(2));
-console.log(usersArray);
+const getUser = (userId) => {
+    if (!userId) {
+        return null;
+    }
+    else {
+        const getingUser = usersArray.filter(user => user.id === userId);
+        return getingUser;
+    }
+};
+exports.getUser = getUser;
+// console.log("getuser", getUser(3))
+const findUser = (userId) => {
+    if (!userId) {
+        return null;
+    }
+    else {
+        const getingUser = usersArray.filter(user => user.id === userId);
+        return getingUser;
+    }
+};
+exports.findUser = findUser;
+const getAllUsers = () => usersArray;
+exports.getAllUsers = getAllUsers;
+// console.log("getalluser", getAllUsers(usersArray));
+const userIdToRemove = 3;
+const removeUser = (userId) => {
+    const removeElement = usersArray.filter(user => user.id === userId);
+    return removeElement;
+};
+exports.removeUser = removeUser;
+console.log("removeuser", (0, exports.removeUser)(userIdToRemove));
+// removeUser(userIdToRemove)
+// console.log(usersArray)
--- src/users/user.ts
+++ src/users/user.ts
@@ -1,41 +1,48 @@
-interface User{
-    id:number;
-    userName:string;
-    email:string;
-    postCount:2
+interface Iuser {
+    id: number;
+    userName: string;
+    email: string;
+    postCount: number
 }
 
 
-const usersArray:{id:number;userName:string;email:string,postCount:number}[]=[
-    {id:1,userName:"shamol",email:"shamol@gmail.com",postCount:2},   
-    {id:2,userName:"shamol2",email:"shamol2@gmail.com",postCount:2},   
-    {id:3,userName:"shamol3",email:"shamol3@gmail.com",postCount:2}   
+const usersArray: Iuser[] = [
+    { id: 1, userName: "shamol", email: "shamol@gmail.com", postCount: 2 },
+    { id: 2, userName: "shamol2", email: "shamol2@gmail.com", postCount: 2 },
+    { id: 3, userName: "shamol3", email: "shamol3@gmail.com", postCount: 2 }
 ]
 
-function getUser(userId: number){
-    const getingUser=usersArray.filter(user=>user.id===userId)
-    return getingUser;
+export const getUser=(userId: number) =>{
+    if (!userId) {
+        return null;
+    } else {
+        const getingUser = usersArray.filter(user => user.id === userId)
+        return getingUser;
+    }
+
 }
 
-console.log("getuser",getUser(3))
+// console.log("getuser", getUser(3))
 
-function findUser(userId: number){
-    const getingUser=usersArray.filter(user=>user.id===userId)
-    return getingUser;
+export const findUser=(userId: number)=> {
+    if (!userId) {
+        return null;
+    } else {
+        const getingUser = usersArray.filter(user => user.id === userId)
+        return getingUser;
+    }
 }
 
-function getAllUser(users:any[]) {
-    return users;
+export const getAllUsers = (): Iuser[] => usersArray;
+
+// console.log("getalluser", getAllUsers(usersArray));
+
+const userIdToRemove = 3;
+export const removeUser = (userId: number)=>{
+   const removeElement= usersArray.filter(user => user.id === userId);
+   return removeElement;
 }
-
-console.log("getalluser",getAllUser(usersArray));
-
-
-function removeUser(userId:number){
-    const remove=usersArray.filter(user=>user.id === userId);
-    return remove;
-}
-
-console.log("removeuser",removeUser(2))
-
-console.log(usersArray)
+   
+console.log("removeuser", removeUser(userIdToRemove))
+// removeUser(userIdToRemove)
+// console.log(usersArray)
