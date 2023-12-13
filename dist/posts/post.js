d620c26982ba2d2d1faa5c62ebb4611125ca4b4b
shamolmojumder <shamolmojumder14@gmail.com>
2023-12-14 01:04:04 +0600

    Create Users example Crud & Posts Example Crud with TypeScript

--- dist/posts/post.js
+++ dist/posts/post.js
@@ -1,1 +1,39 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removepost = void 0;
+const postArray = [
+    { id: 1, title: "title1", description: "lorem lorem", createdAt: {}, createdBy: 1 },
+    { id: 2, title: "title1", description: "lorem lorem", createdAt: {}, createdBy: 2 },
+    { id: 3, title: "title1", description: "lorem lorem", createdAt: {}, createdBy: 3 },
+    { id: 4, title: "title1", description: "lorem lorem", createdAt: {}, createdBy: 4 }
+];
+const getPost = (postId) => {
+    if (!postId) {
+        return null;
+    }
+    else {
+        const gettingPost = postArray.filter(post => post.id === postId);
+        return gettingPost;
+    }
+};
+const findPost = (postId) => {
+    if (!postId) {
+        return null;
+    }
+    else {
+        const findingPost = postArray.filter(post => post.id === postId);
+        return findingPost;
+    }
+};
+console.log("findPOst 1", findPost(1));
+const GettingAllPost = () => {
+    return postArray;
+};
+console.log(GettingAllPost);
+const postIdToRemove = 3;
+const removepost = (postId) => {
+    const removeElement = postArray.filter(user => user.id === postId);
+    return removeElement;
+};
+exports.removepost = removepost;
+console.log((0, exports.removepost)(postIdToRemove));
